ac1cbeef2b22ab640ec47c14a502ae11c8654ab8
felishahardina <161101395+felishahardina@users.noreply.github.com>
2024-07-01 20:28:45 +0700

    Add files via upload

---
+++ KoneksiDb.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package koneksi;
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author ASUS
+ */
+public class KoneksiDb {
+    static Connection con;
+    
+    public static Connection connection(){
+        if(con == null){
+            MysqlDataSource data = new MysqlDataSource();
+            data.setDatabaseName("dbbarang");
+            data.setUser("root");
+            data.setPassword("");
+            
+            try {
+                con = (Connection) data.getConnection();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }  
+        }
+        return con;
+    }
+}
